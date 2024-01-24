72d7a104f5560f961221f04d119632671300789d
Samuel Fletcher <samuelrhfletcher@hotmail.com>
2024-01-24 12:23:41 -0500

    little update

--- src/schema.ts
+++ src/schema.ts
@@ -56,6 +56,7 @@ type AuthPayload {
 
 const resolvers = {
   DateTime: GraphQLDateTime,
+  // Listing resolver
   Listing: {
     id: (parent: Listing) => parent.id,
     product(parent: Listing, args: {}, context: GraphQLContext) {
@@ -77,11 +78,13 @@ const resolvers = {
         .postedBy();
     },
   },
+  // User resolver
   User: {
     // ... other User object type field resolver functions ...
     listings: (parent: User, args: {}, context: GraphQLContext) =>
       context.prisma.user.findUnique({ where: { id: parent.id } }).listings(),
   },
+  // Product resolver
   Product: {
     // ... other User object type field resolver functions ...
     listings: (parent: Product, args: {}, context: GraphQLContext) =>
@@ -90,62 +93,53 @@ const resolvers = {
         .listings(),
   },
   Query: {
+    // Test query
     hello: () => "Hello World!",
+    // Gives list of all listings
     feed: (parent: unknown, args: {}, context: GraphQLContext) =>
       context.prisma.listing.findMany(),
+    // Gives current user
     me(parent: unknown, args: {}, context: GraphQLContext) {
       if (context.currentUser === null) {
         throw new Error("Unauthenticated!");
       }
-
       return context.currentUser;
     },
   },
   Mutation: {
-    // ... other Mutation field resolvers ...
+    // Mutation to sign up new User
     async signup(
       parent: unknown,
       args: { email: string; password: string; name: string },
       context: GraphQLContext
     ) {
-      // 1
       const password = await hash(args.password, 10);
-
-      // 2
       const user = await context.prisma.user.create({
         data: { ...args, password },
       });
-
-      // 3
       const token = sign({ userId: user.id }, APP_SECRET);
-
-      // 4
       return { token, user };
     },
+    // Signs user into website and gives jwt token
     async login(
       parent: unknown,
       args: { email: string; password: string },
       context: GraphQLContext
     ) {
-      // 1
       const user = await context.prisma.user.findUnique({
         where: { email: args.email },
       });
       if (!user) {
         throw new Error("No such user found");
       }
-
-      // 2
       const valid = await compare(args.password, user.password);
       if (!valid) {
         throw new Error("Invalid password");
       }
-
       const token = sign({ userId: user.id }, APP_SECRET);
-
-      // 3
       return { token, user };
     },
+    // Creates a new listing
     async newListing(
       parent: unknown,
       args: { description: string; cost: number; prodid: number },
@@ -154,7 +148,6 @@ const resolvers = {
       if (context.currentUser === null) {
         throw new Error("Unauthenticated!");
       }
-
       const newListing = await context.prisma.listing.create({
         data: {
           description: args.description,
@@ -163,20 +156,21 @@ const resolvers = {
           postedBy: { connect: { id: context.currentUser.id } },
         },
       });
-
       return newListing;
     },
+    // Creates a new product
     async newProd(
       parent: unknown,
       args: { name: string; category: string; file: File },
       context: GraphQLContext
     ) {
+      if (context.currentUser === null) {
+        throw new Error("Unauthenticated!");
+      }
       const credentials = {
         accessKeyId: "peU3s3HTRnG3sikb",
         secretAccessKey: "jvBhdrNxeIRs2QghZzDBVs8RIvxScJwHjgPK7QUB",
       };
-
-      // Create an S3 service client object.
       const s3Client = new S3Client({
         endpoint: "https://s3.tebi.io",
         credentials: credentials,
