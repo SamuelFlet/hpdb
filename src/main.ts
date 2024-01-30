8d32e33c74f7622b0b0b067bddf2f5275c315b14
Samuel Fletcher <samuelrhfletcher@hotmail.com>
2024-01-30 13:36:59 -0500

    cool

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,18 @@ import { createYoga } from "graphql-yoga";
 import { schema } from "./schema";
 import { createContext } from "./context";
 import { graphqlUploadExpress } from "graphql-upload-ts";
+import dotenv from "dotenv";
 
 const app = express();
 
 const yoga = createYoga({ schema, context: createContext });
 
-app.use(yoga.graphqlEndpoint, yoga,);
-app.use('/graphql', graphqlUploadExpress({maxFileSize:100000000, maxFiles:10}))
-app.listen(3000, () => {
-  
+app.use(yoga.graphqlEndpoint, yoga);
+app.use(
+  "/graphql",
+  graphqlUploadExpress({ maxFileSize: 100000000, maxFiles: 10 })
+);
+dotenv.config();
+app.listen(process.env.port, () => {
+  console.log(`Running a GraphQL API server at http://localhost:${process.env.port}/graphql`);
 });
